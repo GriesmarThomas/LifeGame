d2e5ef59c432bfe110efe46ae432b58523774857
Thomas Griesmar <thomas.griesmar@versusmind.eu>
2019-11-07 09:20:03 +0100

    Add project files.

---
+++ LifeGame.Test/LifeGameTester.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace LifeGame.Test
+{
+    [TestClass]
+    public class LifeGameTester
+    {
+        [TestMethod]
+        public void InitializeGameTable()
+        {
+        }
+    }
+}
---
+++ LifeGame/Case.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LifeGame
+{
+    public class Case
+    {
+        public int X { get; set; }
+        public int Y { get; set; }
+
+        public bool isAlive { get; set; }
+    }
+}
